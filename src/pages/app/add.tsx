8b6b0ef1946c5f0d52c24d78a853f0afb1c43b65
Jardaxiiik <75133511+Jardaxiiik@users.noreply.github.com>
2023-10-21 15:04:30 +0200

    Add frontend basics

--- src/pages/app/add.tsx
+++ src/pages/app/add.tsx
@@ -1,9 +1,51 @@
+import { useState } from "react";
 import Layout from "../../components/layouts/Layout";
+import React from "react";
+import {
+  Select,
+  FormLabel,
+  FormControl,
+  Input,
+  NumberInput,
+  NumberIncrementStepper,
+  NumberDecrementStepper,
+  NumberInputStepper,
+  NumberInputField,
+  Card,
+  Button
+} from "@chakra-ui/react";
 
 export default function Add() {
+  const [transactionType, setTransactionType] = React.useState("")
+  const [transactionCount, setTransactionCount] = React.useState("1")
+  
+  function handleSubmit() {}
+  
   return (
     <Layout>
-      <h1>Add</h1>
+      <Card m = {50}>
+      <FormControl pl={50} pr={50} m={5}>
+        <FormLabel>Count:</FormLabel>
+          <Select placeholder='Select option' value ={transactionType}>
+            <option value='option1'>Option 1</option>
+            <option value='option2'>Option 2</option>
+            <option value='option3'>Option 3</option>
+          </Select>
+        </FormControl>
+        <FormControl pl={50} pr={50} m={5}>
+          <FormLabel>Count:</FormLabel>
+          <NumberInput defaultValue={1} min={1} bgGradient="linear(to-t, greay.100, grey.200)" 
+          placeholder='15' onChange={(value) => setTransactionCount(value)}
+          value={transactionCount}>
+            <NumberInputField/>
+            <NumberInputStepper>
+              <NumberIncrementStepper />
+              <NumberDecrementStepper />
+            </NumberInputStepper>
+          </NumberInput>
+        </FormControl>
+        <Button colorScheme='blue' variant='outline' onClick={() => handleSubmit()} maxW={300} m={5} ml={50} >Add New Action</Button>
+      </Card>
     </Layout>
   )
 }
