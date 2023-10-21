9ca445935c5789467558d9344298d10506f59bfe
zbyju <zbynek.jurica@gmail.com>
2023-10-21 13:40:35 +0200

    Improve login page for already logged in user

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -1,26 +1,43 @@
+"use client";
+
 import BigLayout from "../../components/layouts/BigLayout";
 import LoginInput from "../../components/login/LoginInput";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { redirect } from 'next/navigation';
-import { Center, Button, Card, CardHeader, CardBody, CardFooter,Text } from '@chakra-ui/react'
+import { Center, Button, Card, CardHeader, CardBody, CardFooter, Text, Heading, Box, Link } from '@chakra-ui/react'
+import useAuth, { defaultAuth } from "../../hooks/useAuth";
 
 export default function Login() {
   const [username, setUsername] = useState("guest")
   const [password, setPassword] = useState("guest123")
+  const [auth, setAuth] = useState(defaultAuth())
+
+  useEffect(() => {
+    setAuth(useAuth())
+  })
 
   function handleSubmit() {
     localStorage.setItem("username", JSON.stringify(username));
-    //redirect('app');
+    window.location.href = "/app"
   }
   return (
     <BigLayout>
-      <div>
-            <Card variant = 'filled' m = {50} p ={5}>
-            <LoginInput label="Username:" type="username" value={username} onChange={(event) => setUsername(event.target.value)} required placeholder='your username' />
-            <LoginInput label="Password:" type="password" value={password} onChange={(event) => setPassword(event.target.value)} required placeholder='your password' />
-            <Button colorScheme='blue' variant = 'outline' onClick={() => handleSubmit()} maxW = {300} m={5} ml ={100}>Login</Button>
+      {
+        auth.loggedIn ? (
+          <Box>
+            <Heading>Already logged in as: {auth.user.username}</Heading>
+            <Link href="/app">Back to the homepage</Link>
+          </Box>
+        ) : (
+          <div>
+            <Card variant='filled' m={50} p={5}>
+              <LoginInput label="Username:" type="username" value={username} onChange={(event) => setUsername(event.target.value)} required placeholder='your username' />
+              <LoginInput label="Password:" type="password" value={password} onChange={(event) => setPassword(event.target.value)} required placeholder='your password' />
+              <Button colorScheme='blue' variant='outline' onClick={() => handleSubmit()} maxW={300} my={5}>Login</Button>
             </Card>
-      </div>
+          </div>
+        )
+      }
     </BigLayout>
   )
 }
