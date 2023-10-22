f002d180355e92b22a52bfe5f7f7403947ab843c
zbyju <zbynek.jurica@gmail.com>
2023-10-22 10:23:07 +0200

    Add fetching history

--- src/pages/app/history.tsx
+++ src/pages/app/history.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+"use client"
+
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/layouts/Layout";
 import {
   Heading,
@@ -17,40 +19,46 @@ import {
 } from "@chakra-ui/react";
 
 export default function History() {
-  const transactionsHistory = [
-    { id: 1, date: '14.10.2023', transactionType: "Food", score: -2, description: 'it was a 22nice day' },
-    { id: 2, date: '14.10.2023', transactionType: "Drink", score: -1, description: 'it was a n23345ice day' },
-    { id: 3, date: '14.10.2023', transactionType: 'Shower', score: -4, description: 'it was a nice day' },
-    { id: 4, date: '14.10.2023', transactionType: 'Plant a tree', score: 2, description: 'it was a nice day' },
-    { id: 5, date: '14.10.2023', transactionType: 'Public cleaning', score: 2, description: 'it was a nice day' },
-    { id: 6, date: '14.10.2023', transactionType: "Food", score: -2, description: 'it was a nice day' },
-    { id: 7, date: '14.10.2023', transactionType: "Drink", score: -1, description: 'it was a nice day' },
-    { id: 8, date: '14.10.2023', transactionType: 'Shower', score: -4, description: 'it was a nice day' },
-    { id: 9, date: '14.10.2023', transactionType: 'Plant a tree', score: 2, description: 'it was a nice day' },
-    { id: 10, date: '14.10.2023', transactionType: 'Public cleaning', score: 2, description: 'it was a nice day' }
-  ]
+  const [history, setHistory] = useState<any[]>([])
   const [selectedTransaction, setSelectedTransaction] = useState<any | null>(null)
+
+  async function fetchData(userId: number | string) {
+    try {
+      const res = await fetch(`/api/transactions/user/${userId}`)
+      const t = await res.json()
+      setHistory(t)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    try {
+      const id = localStorage.getItem("userid")
+      fetchData(id || "1")
+    } catch (err) { }
+  }, [])
 
   return (
     <Layout>
       <Heading m="5%">Action History</Heading>
       <Box bg='orange.200' h='1em'></Box>
       <Box bg='orange.200' h='15em' maxHeight="15em" w='full' maxW='full'>
-        <Table pl ='5em' pr='2em'   h='1em' maxH='1em' w='full' maxW='full' bg='red.100'>
+        <Table pl='5em' pr='2em' h='1em' maxH='1em' w='full' maxW='full' bg='red.100'>
           <Tr>
-            <Th><Text
+            <Th w="33%"><Text
               fontWeight="bold"
               textTransform="uppercase"
               fontSize="25"
               letterSpacing="wide"
               color="teal.600">Date</Text></Th>
-            <Th><Text
+            <Th w="33%"><Text
               fontWeight="bold"
               textTransform="uppercase"
               fontSize="25"
               letterSpacing="wide"
               color="teal.600">Type</Text></Th>
-            <Th><Text
+            <Th w="33%"><Text
               fontWeight="bold"
               textTransform="uppercase"
               fontSize="25"
@@ -60,46 +68,46 @@ export default function History() {
         </Table>
         <Box overflowY="auto" bg='orange.200' h='14em' maxHeight="14em" w='full' maxW='full'>
           <Grid p='0.5em' alignItems="center" templateColumns='repeat(1, 1fr)' gap={2} h='14em' maxH='14'>
-              {transactionsHistory.map((transaction) => (
-                <GridItem key={transaction.id}>     
-                    <Button onClick={(event)=>{setSelectedTransaction(transaction)}} maxW ="100%" width = "100%" bg="blue.200">
-                      <Table alignItems="center" pt='1em' pb='1em'>
-                        <Tr>
-                          <Th>{transaction.date}</Th>
-                          <Th>{transaction.transactionType}</Th>
-                          <Th>{transaction.score}</Th>
-                        </Tr>
-                      </Table>
-                    </Button>
-                </GridItem>
-                  ))}
+            {history.map((transaction) => (
+              <GridItem key={transaction.tid}>
+                <Button onClick={() => { setSelectedTransaction(transaction) }} maxW="100%" width="100%" bg="blue.200">
+                  <Table alignItems="center" pt='1em' pb='1em'>
+                    <Tr>
+                      <Th w="33%">{(new Date(transaction.tdate)).toUTCString()}</Th>
+                      <Th w="33%">{transaction.cname}</Th>
+                      <Th w="33%">{transaction.cscore}</Th>
+                    </Tr>
+                  </Table>
+                </Button>
+              </GridItem>
+            ))}
           </Grid>
         </Box>
       </Box>
       <Box bg='orange.200' h='2.5em'></Box>
       <Box p={4} display={{ md: "flex" }}>
         <Box mt={{ base: 4, md: 0 }} ml={{ md: 6 }}>
-            <Text
+          <Text
             fontWeight="bold"
             textTransform="uppercase"
             fontSize="25"
             letterSpacing="wide"
             color="teal.600"
-            >
-                {selectedTransaction === null ? 'Nothing selected' : (<>{selectedTransaction.transactionType}</>)}
-            </Text>
-            <Text
+          >
+            {selectedTransaction === null ? 'Nothing selected' : (<>{selectedTransaction.cname}</>)}
+          </Text>
+          <Text
             mt={1}
             display="block"
             fontSize="lg"
             lineHeight="normal"
             fontWeight="semibold"
-            >
-                Description:
-            </Text>
-            {selectedTransaction === null ? (<Text mt={2} color="gray.500"></Text>) : (<Text mt={2} color="gray.500">{selectedTransaction.description}</Text>)}
+          >
+            Description:
+          </Text>
+          {selectedTransaction === null ? (<Text mt={2} color="gray.500"></Text>) : (<Text mt={2} color="gray.500">{selectedTransaction.cdescription}</Text>)}
         </Box>
       </Box>
-    </Layout>
+    </Layout >
   );
 }
